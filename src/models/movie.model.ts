2bd9d98fc1ae457b3bff1c609ee0917dc2a2dcd9
Manuel Torres Magdaleno <manuel_e123@hotmail.com>
2022-06-20 01:21:13 -0500

    models for movie object

---
+++ src/models/movie.model.ts
@@ -0,0 +1,33 @@
+import { Genre } from './genre.model';
+import { ProductionCompany } from './ProductionCompany.model';
+import { ProductionCountry } from './ProductionCountry.model';
+import { SpokenLanguage } from './SpokenLanguage.model';
+
+// movie interface
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: any;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
